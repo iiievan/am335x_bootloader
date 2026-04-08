00355273b0292d5b9bec2e283bdb1888ffa49b84
mbo <iiievan@yandex.ru>
2026-04-08 14:02:04 +0300

    refactor cmake structure

--- cmake/toolchain.cmake
+++ cmake/toolchain.cmake
@@ -11,11 +11,15 @@ set(SIZE arm-none-eabi-size)
 
 set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
 
-set(ARCH_BASE_FLAGS "-mcpu=cortex-a8 -mtune=cortex-a8 -mfloat-abi=hard -mfpu=neon")
+# Базовые флаги архитектуры - используем string для объединения
+set(ARCH_OPTIONS "-mcpu=cortex-a8 -mtune=cortex-a8 -mfloat-abi=hard -mfpu=neon")
 
-set(CMAKE_C_FLAGS_INIT "${ARCH_BASE_FLAGS} -mlong-calls -fdata-sections -funsigned-char -ffunction-sections -Wall -Wextra -g -ffreestanding")
-set(CMAKE_CXX_FLAGS_INIT "${ARCH_BASE_FLAGS} -mlong-calls -fdata-sections -funsigned-char -ffunction-sections -Wall -Wextra -g -ffreestanding -fno-exceptions -fno-rtti -fno-use-cxa-atexit")
-set(CMAKE_ASM_FLAGS_INIT "${ARCH_BASE_FLAGS} -x assembler-with-cpp -g")
+set(COMMON_FLAGS "-mlong-calls -fdata-sections -funsigned-char -ffunction-sections -Wall -Wextra -g -ffreestanding")
 
-# Флаги линковщика - здесь указываем specs
+# Флаги компиляции
+set(CMAKE_C_FLAGS_INIT "${ARCH_OPTIONS} ${COMMON_FLAGS}")
+set(CMAKE_CXX_FLAGS_INIT "${ARCH_OPTIONS} ${COMMON_FLAGS} -fno-exceptions -fno-rtti -fno-use-cxa-atexit")
+set(CMAKE_ASM_FLAGS_INIT "${ARCH_OPTIONS} -x assembler-with-cpp -g")
+
+# Флаги линковщика
 set(CMAKE_EXE_LINKER_FLAGS_INIT "--specs=nano.specs --specs=nosys.specs")
